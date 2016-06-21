ef116cbaa43b7b77e57cc681769354fa66186384
Dragos Cojocari <dragos.cojocari@ro.ibm.com>
2016-06-21 18:44:31 +0300

    Provide updateAnalyticsEngine example.

--- updateAnalyticsEngine.js
+++ updateAnalyticsEngine.js
@@ -12,11 +12,11 @@ var config = require( "./config.js");
 var csrfRequests = require("./csrfRequests.js");
 
 /**
- * Simulates a hazard event for an existing user and shield. Both user and shield must exist and be associated.
+ * Updates the analytics engine with the newly created user and shields.Must be called every time a new user-shield association is created.
  * The connection information is taken from config.js
  */
-var simulateHazard = function(parameters) {
-  console.info("Using the /global/sendPayloadToMQTT REST endpoint to simulate an event...");
+var updateAnalyticsEngine = function(parameters) {
+  console.info("Refreshing the shield engine shield associations ...");
   var reqUrl = config.api + "/global/sendPayloadToMQTT/" + parameters.outputtype + "/" + parameters.devicetype + "/" + parameters.deviceid + "/" + parameters.type;
 
   console.info("Using URL " + reqUrl);
@@ -33,13 +33,13 @@ var simulateHazard = function(parameters) {
     auth: config.credentials
   }, function (error, response, body) {
     if (error) {
-      console.log("\tSimulate hazard failed. Reason is: " + error);
+      console.log("\tRequest failed. Reason is: " + error);
     }
     else if (response.statusCode != 200) {
-      console.error("\tSimulate hazard failed. Reason is: " + response.statusCode);
+      console.error("\tRequest failed. Reason is: " + response.statusCode);
     }
     else {
-      console.log("\nSimulate hazard succeeded.\nThe event can be seen in the dashboard or through the /hazardEvents/all API\n\nIf the event is not visible in the dashboard or through the API you need to refresh the IoT4I analytics engine. Call \n\tnode updateAnalyticsEngine.js");
+      console.log("\nRefresh succeeded.");
     }
 
     if (body) {
@@ -48,24 +48,15 @@ var simulateHazard = function(parameters) {
   });
 };
 
-var hazardParams = {
-  "payload": {
-      "usr": "user1",	// user id must exist ( see createUser.js)
-      "liquid_detected": "true",
-      "policy_id": "123",
-      "temperature":"12",
-      "extra": {
-        "locationDesc": "kitchen",
-        "deviceDesc": "Amazing device"
-      },
-    },
-    "outputtype": "evt",
-    "devicetype": "wink",
-    "deviceid": "wink",
-    "type": "wink"
-  };
+var commandParams = {
+		  "payload": {
+		    },
+		    "outputtype": "evt",
+		    "devicetype": "API",
+		    "deviceid": "API",
+		    "type": "reloadShieldAssociations"
+		  };
 
-
-csrfRequests.requestAPIWithCSRF(simulateHazard, hazardParams);
+csrfRequests.requestAPIWithCSRF(updateAnalyticsEngine, commandParams);
 
 
